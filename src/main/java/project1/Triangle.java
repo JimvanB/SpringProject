5c7f1648f7834908ea9e7fc4d5967f05afb9467a
Jim van Biezen <jvanbiezen@gmail.om>
2016-03-04 23:19:20 +0100

    Klasse punt toegevoegd

--- src/main/java/project1/Triangle.java
+++ src/main/java/project1/Triangle.java
@@ -1,21 +1,42 @@
 package project1;
 
+import javax.swing.*;
+
 /**
  * Created by Jim on 4-3-2016.
  */
 public class Triangle {
 
-    private String type;
+    private Point pointA;
+    private Point pointB;
+    private Point pointC;
 
     public void draw(){
-        System.out.println("triangle drawn "+getType());
+
+        System.out.println(getPointA() + ", " + getPointB() + ", " + getPointC());
+     }
+
+    public Point getPointA() {
+        return pointA;
     }
 
-    public String getType() {
-        return type;
+    public void setPointA(Point pointA) {
+        this.pointA = pointA;
     }
 
-    public void setType(String type) {
-        this.type = type;
+    public Point getPointB() {
+        return pointB;
+    }
+
+    public void setPointB(Point pointB) {
+        this.pointB = pointB;
+    }
+
+    public Point getPointC() {
+        return pointC;
+    }
+
+    public void setPointC(Point pointC) {
+        this.pointC = pointC;
     }
 }
